0342ff888b3b3ab7bc4cfeaabe9e56f527416f1f
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-12 18:40:07 -0300

    Utilização do RestSharp para consumir API

---
+++ Client/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorWASM
+{
+    public static class Configuration
+    {
+        public static string ApiUrl { get; set; } = "https://localhost:7197";
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -6,9 +6,4 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri("https://localhost:7197/")
-});
-
 await builder.Build().RunAsync();
