babde0cf6f146ce88962ff42682f60a7416997bc
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-13 09:13:13 -0300

    Refatoração no CRUD de categorias em uma só tela

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWASM;
+using RestSharp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<RestClient>();
 
 await builder.Build().RunAsync();
